08700dc09770ea160526a5c42c5f3addbb9df3dd
Manav <manavdesai30399@gmail.com>
2020-03-22 18:45:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+import { LoginComponent } from './routes/login/login.component';
+import { ProfileComponent } from './routes/profile/profile.component';
+import { NewsFeedComponent } from './routes/news-feed/news-feed.component';
+import {LoadingBarModule} from '@ngx-loading-bar/core'
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    NewsFeedComponent,
+
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    LoadingBarModule,
+    NgxSkeletonLoaderModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
